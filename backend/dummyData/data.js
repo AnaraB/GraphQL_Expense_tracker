44ea8a592da494b3f2a1eaf47aece528c5056925
Anara <berdali.aygerim9@gmail.com>
2024-10-08 12:08:07 +0400

    first commit

---
+++ backend/dummyData/data.js
@@ -0,0 +1,53 @@
+const users = [
+  {
+    "_id": "1",
+    "username": "jdoe123",
+    "name": "John Doe",
+    "password": "password123",
+    "profilePicture": "profile1.jpg",
+    "gender": "male"
+  },
+  {
+    "_id": "2",
+    "username": "emsmith",
+    "name": "Emily Smith",
+    "password": "emily2024",
+    "profilePicture": "profile2.jpg",
+    "gender": "female"
+  },
+  {
+    "_id": "3",
+    "username": "rwilliams",
+    "name": "Robert Williams",
+    "password": "rob_will_22",
+    "profilePicture": "profile3.jpg",
+    "gender": "male"
+  },
+  {
+    "_id": "4",
+    "username": "alily",
+    "name": "Alice Lily",
+    "password": "alily_pass",
+    "profilePicture": "profile4.jpg",
+    "gender": "female"
+  },
+  {
+    "_id": "5",
+    "username": "mturner99",
+    "name": "Michael Turner",
+    "password": "turner_mike",
+    "profilePicture": "profile5.jpg",
+    "gender": "male"
+  },
+  {
+    "_id": "6",
+    "username": "sarogers",
+    "name": "Sara Rogers",
+    "password": "sara_r_2023",
+    "profilePicture": "profile10.jpg",
+    "gender": "female"
+  }
+]
+
+
+export { users }
