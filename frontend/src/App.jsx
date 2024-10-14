66614b24376f2e2740a5a62bb72521ae6d16e396
Anara <berdali.aygerim9@gmail.com>
2024-10-14 14:32:55 +0400

    react boilerplate, packages and config set up

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+ 
+
+  return (
+    <>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/transaction/:id" element={<TransactionPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/ui/GridBackground.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function GridBackground({children}) {
+  return (
+    (<div
+      className="w-full bg-black text-white bg-grid-white/[0.2] relative">
+      {/* Radial gradient for the container to give a faded look */}
+      <div
+        className="absolute pointer-events-none inset-0 bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      {children}
+    </div>)
+  );
+}
