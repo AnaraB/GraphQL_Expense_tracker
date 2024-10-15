9f4bb3aa2768005a3c4c0848b0250f0b8de27e41
Anara <berdali.aygerim9@gmail.com>
2024-10-15 12:32:26 +0400

    ui design added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,24 @@
 import { Route, Routes } from 'react-router-dom';
-import {LoginPage } from './pages/LoginPage'
-import { HomePage } from './pages/HomePage'
-import {SignUpPage } from './pages/SignUpPage'
-import { TransactionPage } from './pages/TransactionPage'
-import { NotFoundPage} from './pages/NotFoundPage'
+import LoginPage  from './pages/LoginPage'
+import  HomePage  from './pages/HomePage'
+import  SignUpPage   from './pages/SignUpPage'
+import  TransactionPage  from './pages/TransactionPage'
+import  NotFound from './pages/NotFoundPage'
+import Header from './components/ui/Header'
 
 
 function App() {
- 
+ const authUser = true;
 
   return (
     <>
+    {authUser && <Header />}
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignUpPage />} />
       <Route path="/transaction/:id" element={<TransactionPage />} />
-      <Route path="*" element={<NotFoundPage />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
     </>
   )
--- frontend/src/components/ui/GridBackground.jsx
+++ frontend/src/components/ui/GridBackground.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export function GridBackground({children}) {
   return (
     (<div
-      className="w-full bg-black text-white text-4xl bg-grid-white/[0.2] relative">
+      className="w-full bg-black text-white  bg-grid-white/[0.2] relative">
       {/* Radial gradient for the container to give a faded look */}
       <div
         className="absolute pointer-events-none inset-0 bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
